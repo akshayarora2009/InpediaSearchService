7876577a788c572e003216474f54e1170a6cec26
Akshay Arora <akshayarora2009@gmail.com>
2016-12-05 00:09:38 +0530

    Capability to search on custom query

--- InpediaSearchService/es_helper.py
+++ InpediaSearchService/es_helper.py
@@ -5,5 +5,6 @@ es = Elasticsearch()
 
 
 def query_es(query):
-    res = es.search(index=constants.INDEX_NAME, body={"query": {"match_all": {}}})
+    res = es.search(index=constants.INDEX_NAME,
+                    body={"query": {"fuzzy": {"title": {"value": query, "prefix_length": 1, "max_expansions": 100}}}})
     return res['hits']

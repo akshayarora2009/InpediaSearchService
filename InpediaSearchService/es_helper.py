0d0f5f495115cf621d4e303de1bab5352c2ed337
Akshay Arora <akshayarora2009@gmail.com>
2016-12-05 15:58:12 +0530

    Removing unnecessary information from results

--- InpediaSearchService/es_helper.py
+++ InpediaSearchService/es_helper.py
@@ -7,4 +7,18 @@ es = Elasticsearch()
 def query_es(query):
     res = es.search(index=constants.INDEX_NAME,
                     body={"query": {"fuzzy": {"title": {"value": query, "prefix_length": 1, "max_expansions": 100}}}})
-    return res['hits']
+    hits = res['hits']
+    answer = []
+
+    for hit in hits:
+        if '_index' in hit:
+            del hit['_index']
+
+        # TODO: Temporarily removing `related` products information because it is not being used
+        if '_source' in hit:
+            if 'related' in hit['_source']:
+                del hit['_source']['related']
+
+        answer.append(hit)
+
+    return answer
